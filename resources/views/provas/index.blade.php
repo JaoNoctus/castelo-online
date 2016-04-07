bbd30c7ad577024e9e577fd41ea35204948e03d0
João Vítor Dias <joaodias@noctus.org>
2016-04-06 20:32:57 -0400

    Panel heading

--- resources/views/provas/index.blade.php
+++ resources/views/provas/index.blade.php
@@ -7,7 +7,10 @@
 		</div>
 	</div>
 
-	<div class="panel panel-default">
+	<div class="panel panel-primary">
+		<div class="panel-heading">
+            <h1 class="panel-title">Provas</h1>
+        </div>
 		<div class="panel-body">
 			<table class="table table-striped table-hover table-responsive">
 				<thead>
