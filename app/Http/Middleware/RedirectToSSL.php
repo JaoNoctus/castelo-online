ac1247446210ed61fba2535d5a9b85a68fc968f4
João Vítor Dias <joaodias@noctus.org>
2016-06-18 22:24:37 -0300

    Force to SSL

---
+++ app/Http/Middleware/RedirectToSSL.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Castelo\Http\Middleware;
+
+use Closure;
+
+class RedirectToSSL
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+		if (!$request->isSecure() && app()->env != 'local') {
+			return redirect()->secure($request->path());
+		}
+
+        return $next($request);
+    }
+}
