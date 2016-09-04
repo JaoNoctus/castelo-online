62ade64b0e6eda31f0499cc3ba24f7017a152915
João Vítor Dias <joaodias@noctus.org>
2016-09-04 17:23:15 -0300

    ads

---
+++ resources/views/layouts/_ads.blade.php
@@ -0,0 +1,4 @@
+<center>
+    <ins class="adsbygoogle" style="display:block" data-ad-client="ca-pub-6934296242213124" data-ad-slot="6255245692" data-ad-format="auto"></ins>
+    <script> (adsbygoogle = window.adsbygoogle || []).push({}); </script>
+</center>
